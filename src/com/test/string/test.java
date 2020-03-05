6f575e5cd1b95c5f3d2332e7db992bcb9150fa36
YJ <anqyinjin@hotmail.com>
2020-03-05 13:32:22 +0800

    测试代码

--- src/com/test/string/test.java
+++ src/com/test/string/test.java
@@ -1,20 +1,30 @@
 package com.test.string;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 /**
  * @author yj
  * @date: 2018/8/14
  * @time: 15:04
- * @Description:
+ * @Description: 切割字符串
  */
 
 class Test {
     public static void main(String[] args) {
-        String s = "测试优惠券，满100减10";
+        //格式化数值
+        DecimalFormat decimalFormat = new DecimalFormat("###################.##");
+       /* String s = "测试优惠券，满100减10";
         String s1 = s.substring(0,s.indexOf("，满"));
-        System.out.println(s1);
+        System.out.println(s1);*/
+
+
+        BigDecimal a = new BigDecimal(1.01123124142);
+        System.out.println(a);
+        String s = a.toString();
+        System.out.println(decimalFormat.format(a));
     }
+
 
 }
 
